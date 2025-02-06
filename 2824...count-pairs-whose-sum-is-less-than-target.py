58bb8640cd58b9000d751d2e4730da6e1fe93bb0
Ashikur Rahman <iamashik423@gmail.com>
2025-02-07 00:12:23 +0600

    Update 2824...count-pairs-whose-sum-is-less-than-target.py

--- 2824...count-pairs-whose-sum-is-less-than-target.py
+++ 2824...count-pairs-whose-sum-is-less-than-target.py
@@ -2,19 +2,11 @@ class Solution:
     def countPairs(self, nums: List[int], target: int) -> int:
         nums.sort()
         count = 0
-        for i in range(len(nums)-1):
-            lo, hi, found = i+1, len(nums)-1, False
-            while lo <= hi:
-                mid = (lo+hi)//2
-                if nums[i] + nums[mid] < target:
-                    found = True
-                    if mid == len(nums)-1 or nums[i] + nums[mid+1] >= target:
-                        count += mid - i
-                        break
-                    else:
-                        lo = mid + 1
-                else:
-                    hi = mid - 1
-            if found == False:
-                break
+        left, right = 0, len(nums) - 1
+        while left < right:
+            if nums[left] + nums[right] < target:
+                count += right - left
+                left += 1
+            else:
+                right -= 1
         return count
