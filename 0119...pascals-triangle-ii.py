c5096eaf9688c591fb310db663bb7de0e1706c47
Ashikur Rahman <iamashik423@gmail.com>
2025-02-18 11:34:15 +0600

    Update 0119...pascals-triangle-ii.py

--- 0119...pascals-triangle-ii.py
+++ 0119...pascals-triangle-ii.py
@@ -1,10 +1,7 @@
+import math
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        ans = [1]
-        for i in range(rowIndex):
-            temp = [1]
-            for j in range(i):
-                temp.append(ans[j] + ans[j+1])
-            temp.append(1)
-            ans = temp
+        ans = []
+        for i in range(rowIndex+1):
+            ans.append(factorial(rowIndex)//(factorial(i)*factorial(rowIndex-i)))
         return ans
